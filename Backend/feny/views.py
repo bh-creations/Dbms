761335afe686bb430b5f7e1340e3e5956022709b
qdsandy1995 <qdsandy1990s@gmail.com>
2019-04-27 11:40:05 -0400

    Add more quaries

--- Backend/feny/views.py
+++ Backend/feny/views.py
@@ -32,6 +32,7 @@ def signup(request):
 def average_rating(city):
     with connection.cursor() as cursor:
         category = '%Restaurant%'
+        # Finding average restaurant rating of each city
         statement = "SELECT AVG(b.stars) From business b where b.city = %s and b.categories like %s"
         cursor.execute(statement, [city,category])
         row = cursor.fetchone()
@@ -41,10 +42,66 @@ def average_rating(city):
 def budget_restaurant(city):
     with connection.cursor() as cursor:
         category = '%Restaurant%'
-        statement = "SELECT b.name , b.city From business b, VIKAS.attributes a where b.business_id = a.attr_id and a.pricerange = 1 and b.city = %s and b.categories like %s"
+        # Budget Restaurant
+        statement = "SELECT b.name , b.city From business b, attributes a where b.business_id = a.attr_id and a.pricerange = 1 and b.city = %s and b.categories like %s"
         cursor.execute(statement, [city,category])
         row = cursor.fetchone()
         print(row)
     return row
+
+def search_by_cuisine(cuisine):
+    with connection.cursor() as cursor:
+        category = '%Restaurant%'
+        # Search restaurants according to Cuisine
+        statement = "SELECT b.name From business b where b.categories like %s and b.categories like %s"
+        cursor.execute(statement, [category,cuisine])
+        row = cursor.fetchone()
+        print(row)
+    return row
+
+def deliver_restaurant():
+    with connection.cursor() as cursor:
+        category = '%Restaurant%'
+        # Total number of restaurants that make deliveries in each city
+        statement = "SELECT b.city, count(*) From business b, attributes a where B.business_id = a.attr_id and  b.categories like %s and a.delivery = 1 Group by b.city"
+        cursor.execute(statement, [category])
+        row = cursor.fetchone()
+        print(row)
+    return row
+
+def best_pizza(city):
+    with connection.cursor() as cursor:
+        category = '%Pizza%'
+        # Best Pizza places to eat in a particular city(stars >=3)
+        statement = "SELECT b.name From business B where b.categories like %s and b.stars >= 3 and b.city = %s"
+        cursor.execute(statement, [category,city])
+        row = cursor.fetchone()
+        print(row)
+    return row    
+
+def food_alcohol(city):
+    with connection.cursor() as cursor:
+        c1 = '%Restaurants%'
+        c2 = '%Wine%'
+        c3 = '%Spirits%'
+        c4 = '%Beer%'
+        c5 = '%Pubs%'
+        c6 = '%Bars%'
+        # Places that serve food as well as alcohol in a particular city
+        statement = "SELECT b.name From business B where b.city = %s and  b.categories like %s and (b.categories like %s or b.categories like %s or b.categories like %s or b.categories = %s or b.categories = %s)"
+        cursor.execute(statement, [city,c1,c2,c3,c4,c5,c6])
+        row = cursor.fetchone()
+        print(row)
+    return row  
+
+def cheap_hotel(city):
+    with connection.cursor() as cursor:
+        category = '%Hotels%'
+        # Cheap and Decent hotels to stay at in a particular city
+        statement = "SELECT b.name From business B, attributes a where b.business_id = a.attr_id and b.city = %s and b.categories like %s and b.stars >=3 and a.pricerange <= 3"
+        cursor.execute(statement, [city,category])
+        row = cursor.fetchone()
+        print(row)
+    return row 
 # def login(request):
 # 	return render(request, 'accounts/login.html')
